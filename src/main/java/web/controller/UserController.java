d8b7c01b754da0caf576ad1c82f9962a61c20420
Andrew Bogatyryov <trickingegg@gmail.com>
2024-03-14 20:03:23 +0300

    -Можно добавлять пользователей через страницу с помощью кнопки

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,21 +1,31 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.User;
 import web.service.UserService;
 
 @Controller
 public class UserController {
-    final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/users")
-    public String showUsers(Model model) {
+    public String listUsers(Model model) {
         model.addAttribute("users", userService.listUsers());
         return "users";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@RequestParam String firstName,
+                          @RequestParam String lastName,
+                          @RequestParam String email) {
+        User newUser = new User(firstName, lastName, email);
+        userService.add(newUser);
+        return "redirect:/users";
     }
 }
