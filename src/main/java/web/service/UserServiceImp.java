8b01ade395a59cf69f7f57fd61cfecc7ddbf27ee
Andrew Bogatyryov <trickingegg@gmail.com>
2024-03-21 11:42:49 +0300

    -UserServiceImp добавил пометку readOnly=true

--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -21,7 +21,7 @@ public class UserServiceImp implements UserService {
         userDao.add(user);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     @Override
     public List<User> listUsers() {
         return userDao.listUsers();
